020590eb7a29881e7776f215e5cf0415d28feb89
Анна Евграфова <gra-anya@mail.ru>
2022-11-23 11:34:02 +0300

    add jar

---
+++ src/test/java/ru/evgrafova/springbootrestservice/SpringBootRestServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package ru.evgrafova.springbootrestservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBootRestServiceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
